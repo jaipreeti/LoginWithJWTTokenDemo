7be9a4e5263ed15448f34a3f8a3a512c6d8202e7
Jai <jaiprakash.aksu@gmail.com>
2024-01-31 12:25:17 +0530

    Add project files.

---
+++ LoginWithJWTTokenDemo/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginWithJWTTokenDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ValuesController : ControllerBase
+    {
+        static List<Student> gods = new List<Student>()
+        {
+            new Student{ Id=1, Name="Sri Ram" },
+            new Student { Id = 2, Name = "Hanuman Ji" },
+            new Student { Id = 3, Name = "Krishna Ji" }
+        };
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(gods);
+        }
+        [HttpGet("{Id}")]
+        public IActionResult Get(int Id)
+        {
+            return Ok(gods.FirstOrDefault(x => x.Id == Id));
+        }
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
---
+++ LoginWithJWTTokenDemo/Model/Users.cs
@@ -0,0 +1,8 @@
+﻿namespace LoginWithJWTTokenDemo.Model
+{
+    public class Users
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
